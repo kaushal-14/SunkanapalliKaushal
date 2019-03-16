a46fec5274007653958cc139f29169d0d0225508
kaushal-14 <48099265+kaushal-14@users.noreply.github.com>
2019-03-16 23:17:53 +0530

    Update header.php

--- header.php
+++ header.php
@@ -15,45 +15,7 @@
 							<ul class="nav navbar-nav menu_nav ml-auto">
 								<li class="nav-item active"><a class="nav-link" href="index.php">Home</a></li> 
 								<li class="nav-item"><a class="nav-link" href="gallery.php">Gallery</a></li>
-                                <li class="dropdown-submenu nav-item" style="padding-top: 37px;">
-                                    <a class="dropdown-toggle" style="color: black; font-weight: 400;" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                  ACTIVITIES
-                                </a>
-                                
-
-                                        <?php
-
-                                            $db = mysqli_connect('localhost', 'root', '', 'intern');
-
-                                            $result = $db->query("SELECT * FROM category");
-                                                            
-                                            if($result->num_rows > 0){
-                                                echo '<ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">';
-                                                while($row = $result->fetch_assoc()){
-                                                
-                                                    echo ' <li class="dropdown-submenu">';
-                                                    echo '<a  class="dropdown-item" tabindex="-1" href="#">'.$row["category"].'</a>';
-                                                    $sql = "SELECT * FROM subcategory WHERE categoryid='".$row['id']."' ";
-                                                    $collec = $db->query($sql);
-                                                    if($collec->num_rows > 0){
-                                                        echo '<ul class="dropdown-menu" style="margin-left:120px;">';
-                                                        while($r=$collec->fetch_assoc()){
-                                                            echo '<li class="dropdown-item"><a href="webactivites.php?sub='.$r["id"].'">'.$r["subcategory"].'</a></li>';
-                                                        }
-                                                        echo '</ul>';
-                                                    }
-                                                    echo ' </li>';
-                                                } 
-                                                echo '</ul>';
-                                            }
-
-                                        ?>
-                                      
-                                      
-                                        
-                                   
-                                  
-                                </li> 
+                                <li class="nav-item"><a class="nav-link" href="activites.php">Activites</a></li>
 								<li class="nav-item"><a class="nav-link" href="about-us.php">About</a></li>
                                 
 								<li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
